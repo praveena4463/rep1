bb4a61560b5393fec812f7e9f079a9d2d32a948e
praveena <praveenajohanbrown@gmail.com>
2021-07-05 18:38:39 +0530

    script v9

--- instance.tf
+++ instance.tf
@@ -1,9 +1,9 @@
-resource aws_instance "i"{
-count = "3"
-ami = "ami-09e67e426f25ce0d7"
-instance_type = "micro"
-key_name = "terraform"
-tags = {
-Name = "Instance-${count.index + 1}"
-}
+resource "aws_instance" "i" {
+  count = "3"
+  ami = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+  key_name = "terraform"
+  tags {
+    Name = "instances"
+  }
 }
